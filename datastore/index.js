4e80d6e1954f0ec2bc22baa1012e2a8dc3b2f07f
Ryan Gehris <rogehris@gmail.com>
2022-11-14 17:33:03 -0800

    completed counter and create

--- datastore/index.js
+++ datastore/index.js
@@ -3,14 +3,19 @@ const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
 
-var items = {};
-
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    var newFileName = `${exports.dataDir}/${id}.txt`;
+    fs.writeFile(newFileName, text, (err) => {
+      if (err) {
+        console.log('Error, couldn\'t write file');
+      } else {
+        callback(null, { id, text });
+      }
+    });
+  });
 };
 
 exports.readAll = (callback) => {
